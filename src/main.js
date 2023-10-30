5ea5aec0fc3c7fe450b6364651ef35af8c5d089b
Adriana Isabel Rios Mazo <106763204+airiosm@users.noreply.github.com>
2023-10-29 21:28:16 -0500

    Importaciones

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize.js';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(VueAxios,axios).mount('#app')
+//createApp(App).use(router).mount('#app')
